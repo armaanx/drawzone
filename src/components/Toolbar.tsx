7c23f1c12ad1da433f9eb60a3f25238c4e9f0f32
Armaan Mishra <armaanmishra48@gmail.com>
2025-01-09 17:44:51 +0530

    refactor toolbar component to use updated tools and improve state management

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,28 +1,49 @@
+import { useState } from "react";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { Circle, Minus, Square } from "lucide-react";
-import { ElementType } from "./Canvas";
+import { Circle, Minus, MousePointer2, Square } from "lucide-react";
+import { Tools } from "./Canvas";
+
 interface ToolbarProps {
-  setElementType: (elementType: ElementType) => void;
+  setElementType: (elementType: Tools) => void;
+  onClick: () => void;
 }
-const Toolbar = ({ setElementType }: ToolbarProps) => {
+
+const Toolbar = ({ setElementType, onClick }: ToolbarProps) => {
+  const [selectedTool, setSelectedTool] = useState<Tools>(Tools.line);
+
+  const handleValueChange = (value: string | null) => {
+    if (!value) {
+      setSelectedTool(Tools.select);
+      setElementType(Tools.select);
+    } else {
+      setSelectedTool(value as Tools);
+      setElementType(value as Tools);
+    }
+  };
+
   return (
     <ToggleGroup
       type="single"
       size={"lg"}
-      className="shadow-md p-1 rounded-md"
-      defaultValue="line"
-      onValueChange={(value) => setElementType(value as ElementType)}
+      className="shadow-md p-1 rounded-md z-50"
+      value={selectedTool}
+      onValueChange={handleValueChange}
+      onClick={onClick}
     >
-      <ToggleGroupItem value={ElementType.rectangle} className="">
+      <ToggleGroupItem value={Tools.rectangle} className="">
         <Square />
       </ToggleGroupItem>
-      <ToggleGroupItem value={ElementType.line} className="">
+      <ToggleGroupItem value={Tools.line} className="">
         <Minus />
       </ToggleGroupItem>
-      <ToggleGroupItem value={ElementType.circle} className="">
+      <ToggleGroupItem value={Tools.ellipse} className="">
         <Circle />
+      </ToggleGroupItem>
+      <ToggleGroupItem value={Tools.select} className="">
+        <MousePointer2 />
       </ToggleGroupItem>
     </ToggleGroup>
   );
 };
+
 export default Toolbar;
